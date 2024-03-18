ea0a9dd66ffb176ceb80131cab9ea49b6b24a440
Jack-Sunstone <jack@sunstone-systems.com>
2024-03-18 08:29:56 +0000

    Created Login Blank Window.

--- main.py
+++ main.py
@@ -3,3 +3,17 @@ from PySide6.QtCore import *
 from PySide6.QtWidgets import *
 from PySide6.QtGui import *
 
+class loginUI(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Dashboard Login")
+        self.setGeometry(550,300,380,320)
+
+
+app = QApplication([])
+app.setStyle('GTK')
+window = loginUI()
+
+window.show()
+sys.exit(app.exec())
