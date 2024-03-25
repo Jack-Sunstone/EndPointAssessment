24899d765c6bc7853569bf4b02789efbd2e02aa7
Jack-Sunstone <jack@sunstone-systems.com>
2024-03-25 14:41:47 +0000

    added basic connection to SQL Server and Database

--- SQL.py
+++ SQL.py
@@ -1,2 +1,8 @@
 import pyodbc
 
+cnxn = pyodbc.connect(driver="{ODBC Driver 17 for SQL Server}",
+            server="81.179.155.109,1433",
+            database="ARCDashboard",
+            uid="Monitoring",pwd='12Sunstone34')
+
+cursor = cnxn.cursor()
