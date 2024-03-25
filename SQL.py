85e5b3a0f5b897b6b824f6aa47d227deda58f16a
Jack-Sunstone <jack@sunstone-systems.com>
2024-03-25 14:52:06 +0000

    INSERT query added and executed

--- SQL.py
+++ SQL.py
@@ -6,3 +6,8 @@ cnxn = pyodbc.connect(driver="{ODBC Driver 17 for SQL Server}",
             uid="Monitoring",pwd='12Sunstone34')
 
 cursor = cnxn.cursor()
+
+cursor.execute("INSERT INTO dbo.Units (ID, Name, IP, victronID, Location, NoCCTV, Company, Lat, Lon, UnitType) VALUES (01, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL")
+
+
+cnxn.commit()
