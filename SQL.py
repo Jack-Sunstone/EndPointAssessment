b2e0150d73ca5f41af25174c58e72694364199a7
Jack-Sunstone <jack@sunstone-systems.com>
2024-03-26 08:22:16 +0000

    Added SQL Connection

--- SQL.py
+++ SQL.py
@@ -1,2 +1,13 @@
 import pyodbc
 
+cnxn = pyodbc.connect(driver="{ODBC Driver 17 for SQL Server}",
+            server="81.179.155.109,1433",
+            database="ARCDashboard",
+            uid="Monitoring",pwd='12Sunstone34')
+
+cursor = cnxn.cursor()
+
+cursor.execute("INSERT INTO dbo.Units (ID, Name, IP, victronID, Location, NoCCTV, Company, Lat, Lon, UnitType) VALUES (01, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL")
+
+
+cnxn.commit()
