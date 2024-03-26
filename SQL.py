e235fb5d08487a9c4b7d9413fc278221b8832ddd
Jack-Sunstone <jack@sunstone-systems.com>
2024-03-26 08:54:55 +0000

    Added First SQL Function to SELECT all the units that are stored in the database.

--- SQL.py
+++ SQL.py
@@ -7,7 +7,14 @@ cnxn = pyodbc.connect(driver="{ODBC Driver 17 for SQL Server}",
 
 cursor = cnxn.cursor()
 
-cursor.execute("INSERT INTO dbo.Units (ID, Name, IP, victronID, Location, NoCCTV, Company, Lat, Lon, UnitType) VALUES (01, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL")
+#cursor.execute("INSERT INTO dbo.Units (ID, Name, IP, victronID, Location, NoCCTV, Company, Lat, Lon, UnitType) VALUES (01, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL")
 
+def readUnitList():
 
+    cursor.execute("SELECT Name FROM dbo.Units")
+
+    for row in cursor.fetchall():
+        return row[0]
+
+readUnitList()
 cnxn.commit()
