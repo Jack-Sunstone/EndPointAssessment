790ae63c1e026ae517d297932df55014cdb65679
Jack-Sunstone <jack@sunstone-systems.com>
2024-04-04 08:20:04 +0100

    Practice using socket to run a check wether the unit is online

---
+++ practice.py
@@ -0,0 +1,13 @@
+import socket
+def checkURL(IPAddress, Port, Timeout):
+    socketOpen = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    socketOpen.settimeout(Timeout)
+    try:
+        socketOpen.connect((IPAddress, Port))
+    except:
+        return 0
+    else:
+        socketOpen.close()
+        return 1
+
+print(checkURL('000.000.000.000',0000,timeout=1))
