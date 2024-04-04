d56afdd4daa13ffc3e49bb8e8f587130227f87f5
Jack-Sunstone <jack@sunstone-systems.com>
2024-04-04 09:32:15 +0100

    Changed view camera1 function to be reusable within the program so it can be used by each camera

--- main.py
+++ main.py
@@ -143,13 +143,14 @@ class ioDashboard(QWidget):
         self.Camera1.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
         camera1Button = QPushButton("Camera 1")
-        camera1Button.clicked.connect(self.viewCamera1)
+        camera1Button.clicked.connect(lambda checked=None, text=1: self.viewIndividualCamera(text))
 
         self.Camera2 = QLabel()
         self.Camera2.setPixmap(pixmap)
         self.Camera2.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
         camera2Button = QPushButton("Camera 2")
+        camera2Button.clicked.connect(lambda checked=None, text=2: self.viewIndividualCamera(text))
 
         layout.addWidget(self.Camera2, 1, 2)
         layout.addWidget(camera2Button, 2, 2)
@@ -159,6 +160,7 @@ class ioDashboard(QWidget):
         self.Camera3.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
         camera3Button = QPushButton("Camera 3")
+        camera3Button.clicked.connect(lambda checked=None, text=3: self.viewIndividualCamera(text))
 
         layout.addWidget(self.Camera3, 1, 3)
         layout.addWidget(camera3Button, 2, 3)
@@ -168,6 +170,7 @@ class ioDashboard(QWidget):
         self.Camera4.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
         camera4Button = QPushButton("Camera 4")
+        camera4Button.clicked.connect(lambda checked=None, text=4: self.viewIndividualCamera(text))
 
         layout.addWidget(self.Camera4, 1, 4)
         layout.addWidget(camera4Button, 2, 4)
@@ -244,23 +247,23 @@ class ioDashboard(QWidget):
 
         self.setLayout(layout)
 
-    def viewCamera1(self):
+    def viewIndividualCamera(self, cameraNumber):
         if selectedCamera == "Axis":
             if selectedCompany == "WJ":
-                cameraURL = axisPath(wjPassword, selectedIP, 1)
+                cameraURL = axisPath(wjPassword, selectedIP, cameraNumber)
                 threading1Camera(str(cameraURL))
             else:
-                cameraURL = axisPath(sunstonePassword, selectedIP, 1)
+                cameraURL = axisPath(sunstonePassword, selectedIP, cameraNumber)
                 threading1Camera(cameraURL)
         elif selectedCamera == "Hik":
-            cameraURL = hikPath(selectedIP, 1)
+            cameraURL = hikPath(selectedIP, cameraNumber)
             threading1Camera(cameraURL)
         elif selectedCamera == "Hanwha":
             if selectedCompany == "WJ":
-                cameraURL = hanwhaPath(wjPassword, selectedIP, 1)
+                cameraURL = hanwhaPath(wjPassword, selectedIP, cameraNumber)
                 threading1Camera(cameraURL)
             else:
-                cameraURL = hanwhaPath(sunstonePassword, selectedIP, 1)
+                cameraURL = hanwhaPath(sunstonePassword, selectedIP, cameraNumber)
                 threading1Camera(cameraURL)
 
     def openRouter(self):
