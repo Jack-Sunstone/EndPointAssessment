b04ed1e1ae61e82c5a5eb78922c022ab2b92ae59
Jack-Sunstone <jack@sunstone-systems.com>
2024-04-05 13:20:33 +0100

    Practicing storing data from fetch statement

--- practice.py
+++ practice.py
@@ -1,4 +1,5 @@
 import socket
+import SQL
 def checkURL(IPAddress, Port, Timeout):
     socketOpen = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     socketOpen.settimeout(Timeout)
@@ -10,4 +11,22 @@ def checkURL(IPAddress, Port, Timeout):
         socketOpen.close()
         return 1
 
-print(checkURL('000.000.000.000',0000,1))
+#print(checkURL('000.000.000.000',0000,1))
+
+names = []
+
+lat = []
+
+lon = []
+
+data = SQL.fetchLocations()
+
+for row in data:
+    altered = list(row)
+    names.append(altered[0])
+    lat.append(altered[1])
+    lon.append(altered[2])
+
+print(names)
+print(lat)
+print(lon)
