0bc3a76f345dbd3a4e500ef504c330b5d743e175
Jack-Sunstone <jack@sunstone-systems.com>
2024-04-23 11:01:52 +0100

    Created new unit test

--- unitTests.py
+++ unitTests.py
@@ -2,8 +2,6 @@ import unittest
 
 import SQL
 
-#SQL.deleteUnits("NULL")
-
-#class MyTestCase(unittest.TestCase):
- #   def test1(self):
-  #      self.assertRaises(Exception, SQL.updateUnit("NULL","NULL","NULL","NULL"))
+class MyTestCase(unittest.TestCase):
+    def test1(self):
+        self.assertRaises(Exception, SQL.updateUnit("ARC0001","Sunstone","Sunstone","3"))
