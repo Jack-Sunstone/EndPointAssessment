2a2256a931833c75b0434e85e6b7a92984a6ec17
Jack-Sunstone <jack@sunstone-systems.com>
2024-04-25 08:24:56 +0100

    Changes

--- unitTests.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-import SQL
-
-#SQL.deleteUnits("NULL")
-
-#class MyTestCase(unittest.TestCase):
- #   def test1(self):
-  #      self.assertRaises(Exception, SQL.updateUnit("NULL","NULL","NULL","NULL"))
