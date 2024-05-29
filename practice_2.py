51fe8edf1ce1b9631be3ecf2e2021c58201de2e6
Jack-Sunstone <jack@sunstone-systems.com>
2024-05-29 13:10:59 +0100

    Added file

---
+++ practice_2.py
@@ -0,0 +1,20 @@
+import ctypes
+import sys
+import os
+import webbrowser
+import cv2
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5 import QtWebEngineWidgets
+import SQL
+import requests
+import json
+from threading import *
+import socket
+import plotly.graph_objects as go
+from collections import deque
+import time
+import what3words
+
+
